4857148ed8233620df33951d41c57ca9b67f1d32
DoctorHeyde <alexanderrasmuss@gmail.com>
2024-02-26 11:08:12 +0100

    Basic setup in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,15 @@
 package app;
 
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main {
     public static void main(String[] args){
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig("hobby", false);
 
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+        }
     }
 }
