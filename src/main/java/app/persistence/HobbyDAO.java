1425d6a140b84015da3d275ed00388223f3d7400
Laumh-exe <lauritzhauge@gmail.com>
2024-02-26 21:59:15 +0100

    Method finished in HobbyDAO
    Method getNumberOfUsersWithHobby  is completed.
    Need to be tested

--- src/main/java/app/persistence/HobbyDAO.java
+++ src/main/java/app/persistence/HobbyDAO.java
@@ -3,6 +3,8 @@ package app.persistence;
 import app.model.Hobby;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 
 public class HobbyDAO extends DAO<Hobby> {
 
@@ -20,6 +22,15 @@ public class HobbyDAO extends DAO<Hobby> {
        try(EntityManager em = emf.createEntityManager()){
             return em.find(Hobby.class, id);
         }  
+    }
+
+    //getNumberOfUsersWithHobby
+    public int getNumberOfUsersWithHobby(int hobbyId) {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Integer> query = em.createQuery("SELECT h.users.size FROM Hobby h WHERE h.id = :hobbyId", Integer.class);
+            query.setParameter("hobbyId", hobbyId);
+            return query.getSingleResult();
+        }
     }
     
 }
