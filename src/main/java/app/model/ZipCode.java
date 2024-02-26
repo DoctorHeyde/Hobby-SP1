ed695e5a09a1fffd7bee87d34ff25494cf1af734
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-02-26 10:33:00 +0100

    zipcode

--- src/main/java/app/model/ZipCode.java
+++ src/main/java/app/model/ZipCode.java
@@ -1,13 +1,38 @@
 package app.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "zip_code")
+@Table(name = "zipcode")
+@NoArgsConstructor
 public class ZipCode {
+
+    @Id
+    private int zip;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "region_name")
+    private String regionName;
+
+    @Column(name = "municipality_name")
+    private String municipalityName;
+
+
+
+
+
+
+
+
+
 }
