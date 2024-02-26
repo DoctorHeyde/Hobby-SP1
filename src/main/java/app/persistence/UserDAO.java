0a217649ace660b8a9722831152fd4c3b87f44fa
DavidAH <cph-dh223@cphbusiness.dk>
2024-02-26 13:13:48 +0100

    get by zip

--- src/main/java/app/persistence/UserDAO.java
+++ src/main/java/app/persistence/UserDAO.java
@@ -1,8 +1,11 @@
 package app.persistence;
 
+import java.util.List;
+
 import app.model.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 
 public class UserDAO extends DAO<User>{
     public static UserDAO instanse;
@@ -19,5 +22,14 @@ public class UserDAO extends DAO<User>{
         try(EntityManager em = emf.createEntityManager()){
             return em.find(User.class, id);
         }  
+    }
+
+    public List<User> getUsersByZip(int zipCode){
+        try(var em = emf.createEntityManager()){
+            String sql = "SELECT u FROM User u WHERE u.zipCode = :zipCode";
+            TypedQuery<User> q = em.createQuery(sql, User.class);
+            q.setParameter("zipCode", zipCode);
+            return q.getResultList();
+        }
     }
 }
